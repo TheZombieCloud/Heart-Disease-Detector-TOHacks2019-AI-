e0c9cbecef1d1fe3ad62a55972f84d84f002520b
Alexander Ma <alex.magamer@gmail.com>
2019-06-23 02:59:42 -0400

    Euclidean Distance Class Created

--- KNearestNeighbours.py
+++ KNearestNeighbours.py
@@ -1,6 +1,19 @@
 import csv
 import glob
 
+class EuclideanDistance:
+    def __init__(self, distance, classification):
+        self._distance = distance
+        self._classification = classification
+    def getDistance(self):
+        return self._distance
+    def getClassification(self):
+        return self._classification
+
+#default k = 3
+def KNearestNeighbours(data, predict, k=3):
+    dist = []
+
 datasets = {"0":[], "1": [], "3": [], "4": []}
 
 for file in glob.glob("processed.*.data.csv"):
